52021e25d5220050fe2ae257b5b7f6d8edc721d5
Y11o <shakhmardanov.ilnur@mail.ru>
2023-10-12 21:54:14 +0300

    FILTER ON FAVOURITES

--- src/pages/Favourites.vue
+++ src/pages/Favourites.vue
@@ -1,17 +1,35 @@
 <template>
   <v-container fluid>
     <v-pagination
+      v-if="keyWord === ''"
       v-model="pageSelected"
       :length="totalSavedFilmsListPages"
       :total-visible="Math.ceil(totalSavedFilmsListPages / 2) + 1"
       circle
     ></v-pagination>
     <v-row class="flex" align="center">
+      <v-spacer></v-spacer>
+      <v-col cols="3">
+        <v-text-field
+          label="Введите название или описание фильма"
+          v-model="keyWord"
+          clearable
+          @keydown.enter="filterSavedFilms()"
+        ></v-text-field>
+      </v-col>
+      <v-col>
+        <v-btn icon @click="filterSavedFilms()">
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row class="flex" align="center">
       <Film v-for="film in savedFilmsObjOnPage" :film="film" :key="film.id" />
     </v-row>
     <FilmDialog />
     <div class="text-center">
       <v-pagination
+        v-if="keyWord === ''"
         v-model="pageSelected"
         :length="totalSavedFilmsListPages"
         :total-visible="Math.ceil(totalSavedFilmsListPages / 2) + 1"
@@ -31,7 +49,7 @@ export default {
     Film,
     FilmDialog,
   },
-  mounted() {
+  created() {
     this.loadSavedFilmsObj();
     this.loadSaved();
     this.loadRating();
@@ -46,6 +64,7 @@ export default {
   },
   data: () => ({
     savedFilmsObjOnPage: [],
+    keyWord: "",
   }),
   methods: {
     ...mapMutations("films", {
@@ -54,6 +73,33 @@ export default {
       loadRating: "loadRating",
       loadSavedFilmsObj: "loadSavedFilmsObj",
     }),
+    filterSavedFilms() {
+      if (!this.keyWord === "") {
+        let nameRuFilter = this.savedFilmsObj.filter((film) =>
+          film.nameRu.toLowerCase().includes(keyWord.toLowerCase())
+        );
+        let nameEnFilter = this.savedFilmsObj.filter((film) =>
+          film.nameEn.toLowerCase().includes(keyWord.toLowerCase())
+        );
+        console.log(nameRuFilter.concat(nameEnFilter));
+        this.getFilmsOnPage(nameRuFilter.concat(nameEnFilter));
+      } else {
+        this.getFilmsOnPage(this.savedFilmsObj);
+      }
+    },
+    getFilmsOnPage(filmsArray) {
+      if (filmsArray.length < 20 + 20 * (this.currentSavedFilmsPage - 1)) {
+        this.savedFilmsObjOnPage = filmsArray.slice(
+          20 * (this.currentSavedFilmsPage - 1),
+          filmsArray.length
+        );
+      } else {
+        this.savedFilmsObjOnPage = filmsArray.slice(
+          0 + 20 * (this.currentSavedFilmsPage - 1),
+          20 + 20 * (this.currentSavedFilmsPage - 1)
+        );
+      }
+    },
   },
   computed: {
     ...mapState("films", {
@@ -66,18 +112,8 @@ export default {
         return this.currentSavedFilmsPage;
       },
       set(newValue) {
-        if (this.savedFilmsObj.length < 20 + 20 * (newValue - 1)) {
-          this.savedFilmsObjOnPage = this.savedFilmsObj.slice(
-            20 * (newValue - 1),
-            this.savedFilmsObj.length
-          );
-        } else {
-          this.savedFilmsObjOnPage = this.savedFilmsObj.slice(
-            0 + 20 * (newValue - 1),
-            20 + 20 * (newValue - 1)
-          );
-        }
         this.changePageSaved(newValue);
+        this.getFilmsOnPage(this.savedFilmsObj);
       },
     },
   },
